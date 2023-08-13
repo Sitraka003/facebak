dd67017775833e76cddce06e1853000f2712b265
Lalaina0904 <lalainap19.aps2b@gmail.com>
2023-08-13 10:15:59 +0300

    🚀 feat : add contents of each page

--- src/Components/contents/communityContent.jsx
+++ src/Components/contents/communityContent.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import CommunityCards from "../communityCards/communityCards.jsx";
 
 const communityContent = () => {
-    return <div>community</div>;
+    return (
+        <div className="container">
+            {/* une section qui permettra à l'utilisateur de découvrir && || joindre une communauté */}
+
+            <div>
+                <h1>Discover communities</h1>
+                <div>
+                    <div>
+                        <h2>Communities you may like</h2>
+                        <CommunityCards />
+                    </div>
+                </div>
+            </div>
+
+            {/* une section pour consulter les communauté auxquelq il appartient déjà */}
+        </div>
+    );
 };
 
 export default communityContent;
