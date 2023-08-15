8a64c6855f30ca5e332974e63bccc7ad84db9bba
= <=>
2023-08-15 19:01:25 +0300

    feat: add Landing.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import SignUp from "./components/Home/SignUp";
+import SignUp from "./components/SignUp";
+import Landing from "./components/Landing";
 
 export default function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<SignUp />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/sign-up" element={<SignUp />} />
       </Routes>
     </div>
   );
