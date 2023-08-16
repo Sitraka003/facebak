2f12b5ac34674944585535de96c413f109fa339b
Lalaina0904 <lalainap19.aps2b@gmail.com>
2023-08-16 23:07:46 +0300

    🚧 feat/updates : modify - form login

--- src/Components/formLogin/formLogin.jsx
+++ src/Components/formLogin/formLogin.jsx
@@ -9,23 +9,17 @@ const FormLogin = () => {
     return (
         <div>
             <section className="bg-black">
-                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 ">
-                    {/* <a
-                        href="#"
-                        className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-                    >
-                        <Image src={logo} alt="logo" width={70} height={70} />
-                    </a> */}
-                    <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 bg-opacity-10 border-gray-700">
-                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-50 md:text-2xl dark:text-white">
+                <div className="flex flex-col items-center justify-center px-0 md:px-6 py-8 mx-auto h-screen lg:py-0 ">
+                    <div className="rounded-lg shadow sm:border md:mt-0 sm:max-w-[35rem] xl:p-0 bg-gray-800 bg-opacity-10 border-gray-700 w-full">
+                        <div className="m-6 sm:p-6 space-y-4 md:space-y-6 overflow-x-hidden">
+                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-50 md:text-2xl dark:text-white text-center pb-5">
                                 Sign in to your account
                             </h1>
                             <form className="space-y-4 md:space-y-6" action="#">
-                                <div>
+                                <div className="flex flex-col justify-center items-center">
                                     <label
                                         for="email"
-                                        className="block mb-2 text-sm font-medium text-gray-50 dark:text-white"
+                                        className="block mb-2 text-sm font-medium text-gray-50 dark:text-white w-full"
                                     >
                                         Your email
                                     </label>
@@ -33,15 +27,14 @@ const FormLogin = () => {
                                         type="email"
                                         name="email"
                                         id="email"
-                                        className="border text-gray-100 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-200 dark:text-white focus:ring-gray-200 focus:border-gray-200"
-                                        placeholder="name@stellar.com"
-                                        required=""
+                                        className="border text-gray-100 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2.5 bg-gray-700 border-gray-600 placeholder-gray-200 dark:text-white focus:ring-gray-200 focus:border-gray-200 w-[-webkit-fill-available]"
+                                        placeholder="name@gmail.com"
                                     />
                                 </div>
-                                <div>
+                                <div className="flex flex-col justify-center items-center">
                                     <label
                                         for="password"
-                                        className="block mb-2 text-sm font-medium text-gray-50 dark:text-white"
+                                        className="block mb-2 text-sm font-medium text-gray-50 dark:text-white w-full"
                                     >
                                         Password
                                     </label>
@@ -50,8 +43,7 @@ const FormLogin = () => {
                                         name="password"
                                         id="password"
                                         placeholder="••••••••"
-                                        className="border text-gray-100 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-200 dark:text-white focus:ring-gray-200 focus:border-gray-200"
-                                        required=""
+                                        className="border text-gray-100 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2.5 bg-gray-700 border-gray-600 placeholder-gray-200 dark:text-white focus:ring-gray-200 focus:border-gray-200 w-[-webkit-fill-available]"
                                     />
                                 </div>
                                 <div className="flex items-center justify-between">
@@ -62,7 +54,6 @@ const FormLogin = () => {
                                                 aria-describedby="remember"
                                                 type="checkbox"
                                                 className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                                                required=""
                                             />
                                         </div>
                                         <div className="ml-3 text-sm">
@@ -81,12 +72,14 @@ const FormLogin = () => {
                                         Forgot password?
                                     </a>
                                 </div>
-                                <button
-                                    type="submit"
-                                    className="w-full text-gray-900 bg-gray-50 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-                                >
-                                    Sign in
-                                </button>
+                                <div className="w-full flex">
+                                    <button
+                                        type="submit"
+                                        className="w-full text-gray-900 bg-gray-50 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                                    >
+                                        Sign in
+                                    </button>
+                                </div>
 
                                 {/* - register redirection link - */}
                                 <p className="mb-0 mt-5 pt-1 text-sm font-semibold text-white">
@@ -119,7 +112,7 @@ const FormLogin = () => {
                                             data-te-ripple-color="light"
                                             className="mx-1  text-white hover:bg-gray-400 "
                                         >
-                                            <AiOutlineGoogle />
+                                            <AiOutlineGoogle className="hidden sm:block" />
                                         </button>
                                     </div>
 
@@ -130,7 +123,9 @@ const FormLogin = () => {
                                                 to="/"
                                                 className="text-white  flex gap-5 items-center text-[0.8rem]"
                                             >
-                                                <span>Back to home</span>
+                                                <span className="hidden sm:block">
+                                                    Back to home
+                                                </span>
                                                 <BsChevronRight />
                                             </Link>
                                         </p>
