660038e3601bc94b57dc0d8012c6eb13c27a1c86
Astna17 <hei.antsa.17@gmail.com>
2023-08-29 09:59:33 +0200

    feat:"add profil page"

--- src/modules/profil/profil.jsx
+++ src/modules/profil/profil.jsx
@@ -1,60 +1,13 @@
-import React, { useState, useEffect } from "react";
 import "./profil.css";
 import user from "../../assets/teste-files/profil.jpeg";
 
-export function Profil() {
-  const [userData, setUserData] = useState([]);
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    bio: ""
-  });
 
-  useEffect(() => {
-    // Fetch user data from server
-    fetch("http://localhost:8080/users")
-      .then((response) => response.json())
-      .then((data) => setUserData(data))
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
+export function Profil () {
+    return (
+        <div className="profile-container">
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    // Perform POST request with formData
-    fetch("http://localhost:8080/users", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(formData)
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // Handle response or update UI
-        console.log("Data sent successfully:", data);
-        // You might want to update the user data after successful POST
-        setUserData([...userData, data]);
-      })
-      .catch((error) => console.error("Error sending data:", error));
-  };
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  };
-
-  return (
-    <div className="profile-container">
-      <div id="both">
-        <form onSubmit={handleSubmit}>
-          <div className="tab-content" id="v-pills-tabContent">
-            <form action="">
+        <div id="both">
+          <form action="">
           <div className="tab-content" id="v-pills-tabContent">
             <div className="tab-pane fade show active" id="account" role="tabpanel" aria-labelledby="account-tab">
                   <div className="row">
@@ -102,18 +55,8 @@ export function Profil() {
             </div>
           </div>
           </form>
-          </div>
-          <div className="button">
-            <button className="btn btn-primary" type="submit">
-              Update
-            </button>
-            <button className="btn btn-light">Cancel</button>
-          </div>
-        </form>
-
-        <form action="" className="pic">
-          {/* Your image upload code */}
-          <div className="settings">            
+            <form action="" className="pic">
+              <div className="settings">            
                 <h3>Account Settings</h3>
               </div>
             <div className="upload">
@@ -122,17 +65,8 @@ export function Profil() {
             <input type="file" />
               </div>
               </div>
-        </form>
-
-        {/* User data display */}
-        <div className="user-list">
-          {userData.map((user) => (
-            <div key={user.id} className="user-card">
-              {/* User data display */}
-            </div>
-          ))}
+            </form>
         </div>
-      </div>
-    </div>
-  );
+        </div>
+    )
 }
